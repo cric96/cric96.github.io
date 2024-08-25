755dcd8da6b9449b82a9735ead1581ec9a01403d
Gianluca Aguzzi <gianluca.aguzzi@studio.unibo.it>
2024-08-25 18:14:10 +0200

    adjust the website

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 
 
-export const SITE_NAME = 'Gianluca Aguzzi Personal Site'
+export const SITE_NAME = 'Gianluca Aguzzi - PhD'
 
-export const DESCRIPTION = 'This is my personal site in which I share my thoughts and experiences in Academia'
+export const DESCRIPTION = 'Hello! This is a personal website which contains information about my research, publications, and projects. '
