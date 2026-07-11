cb2023fcb3e939d296af34c90abd400bbd826f25
Gianluca Aguzzi <gianluca.aguzzi@unibo.it>
2026-07-11 08:10:23 +0200

    update website

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
 import mdx from '@astrojs/mdx';
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon(), mdx()]
+  integrations: [icon(), mdx()]
 });
